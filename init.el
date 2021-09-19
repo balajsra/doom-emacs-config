4d5cf85fac6be9069a14e0a89363a31399a2d258
Sravan Balaji <balajsra@umich.edu>
2021-09-19 08:55:29 -0400

    Enable LSP Mode
    - Enable lsp mode flag for supported programming languages

--- init.el
+++ init.el
@@ -98,7 +98,7 @@ tty               ; improve the terminal Emacs experience
 :lang
 ;;agda              ; types of types of types of types...
 ;;beancount         ; mind the GAAP
-cc                ; C > C++ == 1
+(cc +lsp)                ; C > C++ == 1
 ;;clojure           ; java with a lisp
 ;;common-lisp       ; if you've seen one lisp, you've seen them all
 ;;coq               ; proofs-as-programs
@@ -123,8 +123,8 @@ emacs-lisp        ; drown in parentheses
 ;;idris             ; a language you can depend on
 json              ; At least it ain't XML
 ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
-javascript        ; all(hope(abandon(ye(who(enter(here))))))
-julia             ; a better, faster MATLAB
+(javascript +lsp)        ; all(hope(abandon(ye(who(enter(here))))))
+(julia +lsp)             ; a better, faster MATLAB
 ;;kotlin            ; a better, slicker Java(Script)
 latex             ; writing papers in Emacs has never been so fun
 ;;lean              ; for folks with too much to prove
@@ -138,7 +138,7 @@ org               ; organize your plain life in plain text
 ;;php               ; perl's insecure younger brother
 ;;plantuml          ; diagrams for confusing people more
 ;;purescript        ; javascript, but functional
-python            ; beautiful is better than ugly
+(python +lsp)            ; beautiful is better than ugly
 ;;qt                ; the 'cutest' gui framework ever
 ;;racket            ; a DSL for DSLs
 ;;raku              ; the artist formerly known as perl6
@@ -148,12 +148,12 @@ python            ; beautiful is better than ugly
 ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
 ;;scala             ; java, but good
 ;;(scheme +guile)   ; a fully conniving family of lisps
-sh                ; she sells {ba,z,fi}sh shells on the C xor
+(sh +lsp)                ; she sells {ba,z,fi}sh shells on the C xor
 ;;sml
 ;;solidity          ; do you need a blockchain? No.
 ;;swift             ; who asked for emoji variables?
 ;;terra             ; Earth and Moon in alignment for performance.
-web               ; the tubes
+(web +lsp)               ; the tubes
 yaml              ; JSON, but readable
 ;;zig               ; C, but simpler
 
