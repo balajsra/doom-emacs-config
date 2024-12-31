a3624d9371cb3dde6e8dadc1fe28d80446e24c09
Sravan Balaji <balajsra@umich.edu>
2024-12-31 10:20:46 -0500

    Update external language support packages
    - Remove pkgbuild (arch packages) mode
    - Remove yuck (for EWW configs) mode
    - Add portage modes

--- packages.el
+++ packages.el
@@ -2,16 +2,17 @@
 ;;; $DOOMDIR/packages.el
 
 (package! org-auto-tangle)
+
 (package! git-modes)
 (package! vimrc-mode)
 (package! prettier-js)
 (package! rainbow-mode)
 (package! systemd)
-(package! pkgbuild-mode)
 (package! lua-mode)
 (package! just-mode)
 (package! nix-mode)
-(package! yuck-mode)
-(package! ebuild-mode)
 (package! sxhkd-mode
   :recipe (:host github :repo "xFA25E/sxhkd-mode"))
+(package! ebuild-mode)
+(package! portage-modes
+  :recipe (:host github :repo "OpenSauce04/portage-modes"))
