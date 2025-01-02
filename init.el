8004d554b934b04fd0040a672dd625cfd96ccc27
Sravan Balaji <balajsra@umich.edu>
2025-01-02 09:37:23 -0500

    Enable editorconfig support

--- init.el
+++ init.el
@@ -78,7 +78,7 @@ grammar                                  ; tasing grammar mistake every you make
 (debugger +lsp)  ; FIXME stepping through code, to help you add bugs
 ;;direnv
 (docker +lsp)
-;;editorconfig   ; let someone else argue about tabs vs spaces
+editorconfig     ; let someone else argue about tabs vs spaces
 ein              ; tame Jupyter notebooks with emacs
 (eval +overlay)  ; run code, run (also, repls)
 ;;gist           ; interacting with github gists
